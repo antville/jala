228778bc57c9d274e0486e09ff3b08ea30ad0232
Robert Gaggl <robert@nomatic.org>
2007-02-27 11:15:34 +0000

    completely rewrote calendar rendering: * the former renderCalendar method is now a constructor jala.Date.Calendar, providing various getter/setter methods for locale, timezone, renderer etc. while this implementation still relies on a grouped HopObject collection, the format of the groupnames used to determine whether a day in the calendar should be rendered as link is now settable using setAccessNameFormat(). plus the format of the links rendered is customizable too using setHrefFormat(). * added a default renderer jala.Date.Calendar.Renderer as constructor. this way the renderer methods appear in the api documentation, and it allows custom renderers to extend the default one.
    git-svn-id: http://dev.orf.at/source/jala/trunk@127 f3d1f554-2238-0410-8348-9ec7ac89951c

--- code/Date.js
+++ code/Date.js
@@ -22,13 +22,20 @@
 //
 
 /**
- * @fileoverview Fields and methods of the jala.Renderings class.
+ * @fileoverview Fields and methods of the jala.Date class.
  */
 
 // Define the global namespace for Jala modules
 if (!global.jala) {
    global.jala = {};
 }
+
+
+/**
+ * HelmaLib dependencies
+ */
+app.addRepository("modules/core/Date.js");
+app.addRepository("modules/helma/Html.js");
 
 /**
  * Constructs a new Renderings object.
@@ -136,75 +143,203 @@ jala.Date.prototype.renderEditorAsString = function(prefix, date, pattern) {
 };
 
 /**
- * Renders a calendar based on a grouped collection of HopObjects. This method
- * uses a rendering object passed as arguments, that <em>must</em> implement
- * the following methods:
- * <ul>
- * <li>navigation</li>
- * <li>dayheader</li>
- * <li>day</li>
- * <li>week</li>
- * <li>calendar</li>
- * </ul>
- * For a description of the arguments passed to these methods please
- * see yourself in the source code of this method.
+ * Creates a new instance of jala.Data.Calendar
+ * @class This class represents a calendar based based on a grouped
+ * collection of HopObjects. It provides several methods for rendering
+ * the calendar plus defining locale and timezone settings.
  * @param {HopObject} collection A grouped HopObject collection to work on
- * @param {Object} renderer A Renderer object which contains the rendering methods listed above
- * @param {String} today A string to highlight in calendar (in format 'yyyyMMdd')
+ * @returns A newly created jala.Date.Calendar instance
+ * @constructor
  */
-jala.Date.prototype.renderCalendar = function(collection, renderer, today) {
+jala.Date.Calendar = function(collection) {
+   var renderer = null;
+   var locale = java.util.Locale.getDefault();
+   var timezone = java.util.TimeZone.getDefault();
+   var hrefFormat = "yyyyMMdd";
+   var accessNameFormat = "yyyyMMdd";
+
+   /**
+    * Returns the collection this calendar object works on
+    * @returns The HopObject collection of this calendar
+    * @type HopObject
+    */
+   this.getCollection = function() {
+      return collection;
+   };
+
+   /**
+    * Sets the renderer to use.
+    * @param {Object} r The renderer to use
+    * @see #getRenderer
+    */
+   this.setRenderer = function(r) {
+      renderer = r;
+      return;
+   };
+
+   /**
+    * Returns the renderer used by this calendar.
+    * @returns The calendar renderer
+    * @type Object
+    * @see #setRenderer
+    */
+   this.getRenderer = function() {
+      if (!renderer) {
+         renderer = new jala.Date.Calendar.Renderer(this);
+      }
+      return renderer;
+   };
+
+   /**
+    * Sets the locale to use within this calendar object
+    * @param {java.util.Locale} loc The locale to use
+    * @see #getLocale
+    */
+   this.setLocale = function(loc) {
+      locale = loc;
+      return;
+   };
+
+   /**
+    * Returns the locale used within this calendar instance.  By default
+    * the locale used by this calendar is the default locale of the
+    * Java Virtual Machine running Helma.
+    * @returns The locale of this calendar
+    * @type java.util.Locale
+    * @see #setLocale
+    */
+   this.getLocale = function() {
+      return locale;
+   };
+
+   /**
+    * Sets the locale to use within this calendar object
+    * @param {java.util.Locale} loc The locale to use
+    * @see #getTimeZone
+    */
+   this.setTimeZone = function(tz) {
+      timezone = tz;
+      return;
+   };
+
+   /**
+    * Returns the locale used within this calendar instance. By default
+    * the timezone used by this calendar is the default timezone
+    * of the Java Virtual Machine running Helma.
+    * @returns The locale of this calendar
+    * @type java.util.Locale
+    * @see #setTimeZone
+    */
+   this.getTimeZone = function() {
+      return timezone;
+   };
+
+   /**
+    * Sets the format of the hrefs to render by this calendar
+    * to the format pattern passed as argument.
+    * @param {String} fmt The date format pattern to use for
+    * rendering the href
+    * @see #getHrefFormat
+    */
+   this.setHrefFormat = function(fmt) {
+      hrefFormat = fmt;
+      return;
+   };
+
+   /**
+    * Returns the date formatting pattern used to render hrefs. The default
+    * format is "yyyyMMdd".
+    * @returns The date formatting pattern
+    * @type String
+    * @see #setHrefFormat
+    */
+   this.getHrefFormat = function() {
+      return hrefFormat;
+   };
+
+   /**
+    * Sets the format of the group name to use when trying to access
+    * child objects of the collection this calendar is operating on.
+    * @param {String} fmt The date format pattern to use for
+    * accessing child objects
+    * @see #getAccessNameFormat
+    */
+   this.setAccessNameFormat = function(fmt) {
+      accessNameFormat = fmt;
+      return;
+   };
+
+   /**
+    * Returns the format of the access name used by this calendar to access
+    * child group objects of the collection this calendar is operating on.
+    * The default format is "yyyyMMdd".
+    * @returns The date formatting pattern used to access child objects
+    * @type String
+    * @see #setAccessNameFormat
+    */
+   this.getAccessNameFormat = function() {
+      return accessNameFormat;
+   };
+
+   return this;
+};
+
+/** @ignore */
+jala.Date.Calendar.prototype.toString = function() {
+   return "[Jala Calendar]";
+};
+
+/**
+ * Renders the calendar using either a custom renderer defined
+ * using {@link #setRenderer} or the default one.
+ * @see #setRenderer
+ * @see jala.Date.Calendar.Renderer
+ */
+jala.Date.Calendar.prototype.render = function(today) {
+   var renderer = this.getRenderer();
+   var collection = this.getCollection();
+   var hrefFormat = this.getHrefFormat();
+   var accessNameFormat = this.getAccessNameFormat();
+   var locale = this.getLocale();
+   var timezone = this.getTimeZone();
    var size = collection.size();
    if (size == null)
       return;
 
    /**
-    * private method for rendering the links to
-    * previous/next month
+    * private method that creates a date object set
+    * to the last date of the previous month or the
+    * first date of the next month (if available)
     */
-   var linkToMonth = function(which, dayIndex) {
-      var d;
-      switch (which) {
-         case "prev":
-         if (size <= dayIndex)
+   var prevNextMonth = function(which, dayIndex) {
+      var obj;
+      if (which == "prev") {
+         if (size <= dayIndex || !(obj = collection.get(dayIndex +1)))
             return;
-         if (!(d = collection.get(dayIndex +1)))
+      } else if (which == "next") {
+         if (dayIndex == 0 || !(obj = collection.get(dayIndex - 1)))
             return;
-         break;
-
-         case "next":
-         if (dayIndex == 0)
-            return;
-         if (!(d = collection.get(dayIndex - 1)))
-            return;
-         break;
+      } else {
+         return;
       }
-      date.setFullYear(d.groupname.substring(0, 4),
-                    d.groupname.substring(4, 6) -1,
-                    d.groupname.substring(6));
-      return renderer.navigation({day: d,
-                date: date,
-                text: monthNames[d.groupname.substring(4, 6) -1]},
-                collection);
-   }
+      return new Date(obj.groupname.substring(0, 4),
+                      obj.groupname.substring(4, 6) -1,
+                      obj.groupname.substring(6));
+   };
 
-   // define variables needed in this function
-   var calParam = {};
-   var dayParam = {};
-   var cal = java.util.Calendar.getInstance();
+   // create the calendar object used for date calculations
+   var cal = java.util.Calendar.getInstance(timezone, locale);
    var firstDayOfWeek = cal.getFirstDayOfWeek();
+   var symbols = new java.text.DateFormatSymbols(locale);
 
-   var symbols = new java.text.DateFormatSymbols();
+   res.push();
+   // render the header-row
+   res.push();
    var weekdays = symbols.getShortWeekdays();
-   var monthNames = symbols.getMonths();
-
-   res.push();
-   // header-row
-   res.push();
    for (var i=0;i<7;i++) {
-      dayParam.text = weekdays[(i+firstDayOfWeek-1)%7+1];
-      renderer.dayheader(dayParam, collection);
+      renderer.renderDayHeader(weekdays[(i+firstDayOfWeek-1)%7+1]);
    }
-   renderer.week({week: res.pop()}, collection);
+   renderer.renderRow(res.pop());
 
    cal.set(java.util.Calendar.DATE, 1);
    // check whether there's a day in path
@@ -219,46 +354,41 @@ jala.Date.prototype.renderCalendar = function(collection, renderer, today) {
    var weeks = Math.ceil((pre + days) / 7);
    var daycnt = 1;
 
-   calParam.month = monthNames[cal.get(java.util.Calendar.MONTH)];
-   calParam.year =  cal.get(java.util.Calendar.YEAR);
-
-   // pre-render the year and month so we only have to append the days as we loop
    var date = cal.getTime();
    // remember the index of the first and last days within this month.
    // this is needed to optimize previous and next month links.
-   var lastDayIndex = 9999999;
+   var lastDayIndex = Number.MAX_VALUE;
    var firstDayIndex = -1;
 
+   var dayObj, idx, selected;
    for (var i=0;i<weeks;i++) {
       res.push();
       for (var j=0;j<7;j++) {
-         if ((i == 0 && j < pre) || daycnt > days)
-            dayParam.text = null;
-         else {
+         if ((i == 0 && j < pre) || daycnt > days) {
+            renderer.renderDay(null);
+         } else {
             date.setDate(daycnt);
-            dayParam.text = daycnt;
-            dayParam.day = collection.get(date.format(GROUPFORMAT));
-            if (dayParam.day) {
-               var idx = collection.contains(dayParam.day);
+            if ((dayObj = collection.get(date.format(accessNameFormat))) != null) {
+               idx = collection.contains(dayObj);
                if (idx > -1) {
-                  if  (idx > firstDayIndex)
+                  if  (idx > firstDayIndex) {
                      firstDayIndex = idx;
-                  if (idx < lastDayIndex)
+                  }
+                  if (idx < lastDayIndex) {
                      lastDayIndex = idx;
+                  }
                }
             }
-            dayParam.date = date;
-            dayParam.selected = today ? date.equals(today) : false;
+            selected = (today != null) ? date.equals(today) : false;
+            renderer.renderDay(date, dayObj != null, selected);
             daycnt++;
          }
-         renderer.day(dayParam, collection);
       }
-      renderer.week({week: res.pop()}, collection);
+      renderer.renderRow(res.pop());
    }
-   calParam.back = linkToMonth("prev", firstDayIndex);
-   calParam.forward = linkToMonth("next", lastDayIndex);
-   calParam.calendar = res.pop();
-   renderer.calendar(calParam, collection);
+   var prevMonth = prevNextMonth("prev", firstDayIndex) || null;
+   var nextMonth = prevNextMonth("next", lastDayIndex) || null;
+   renderer.renderCalendar(date, res.pop(), prevMonth, nextMonth);
    return;
 };
 
@@ -267,10 +397,142 @@ jala.Date.prototype.renderCalendar = function(collection, renderer, today) {
  * @see #renderCalendar
  * @type String
  */
-jala.Date.prototype.renderCalendarAsString = function(collection, renderer, today) {
+jala.Date.Calendar.prototype.getCalendar = function(today) {
    res.push();
-   this.renderCalendar(collection, renderer, today);
+   this.render(today);
    return res.pop();
+};
+
+/**
+ * Returns a new instance of the default calendar renderer.
+ * @class A default renderer to use in conjunction with jala.Date.Calendar
+ * @param {jala.Date.Calendar} calendar The calendar utilizing this renderer
+ * @returns A newly created instance of jala.Date.Calendar.Renderer
+ * @constructor
+ */
+jala.Date.Calendar.Renderer = function(calendar) {
+
+   /**
+    * An instance of helma.Html used for rendering the calendar
+    * @type helma.Html
+    */
+   this.html = new helma.Html();
+
+   /**
+    * The calendar utilizing this renderer instance
+    * @type jala.Date.Calendar
+    */
+   this.calendar = calendar;
+
+   return this;
+};
+
+/** @ignore */
+jala.Date.Calendar.Renderer.prototype.toString = function() {
+   return "[Jala Calendar Default Renderer]";
+};
+
+/**
+ * Renders a single cell in the calendar day header row directly to response.
+ * @param {String} text The text to display in the header field.
+ */
+jala.Date.Calendar.Renderer.prototype.renderDayHeader = function(text) {
+   this.html.element("th", text);
+   return;
+};
+
+/**
+ * Renders a single calendar row directly to response.
+ * @param {String} row The body of the calendar row.
+ */
+jala.Date.Calendar.Renderer.prototype.renderRow = function(row) {
+   this.html.element("tr", row);
+   return;
+};
+
+/**
+ * Renders a single day within the calendar directly to response.
+ * @param {Date} date A date instance representing the day within the calendar.
+ * @param {Boolean} isExisting True if there is a child object in the calendar's
+ * collection to which the date cell should link to
+ * @param {Boolean} isSelected True if this calendar day should be rendered
+ * as selected day.
+ */
+jala.Date.Calendar.Renderer.prototype.renderDay = function(date, isExisting, isSelected) {
+   var attr = {"class": "day"};
+   if (isSelected === true) {
+      attr["class"] += " selected";
+   }
+   this.html.openTag("td", attr);
+   if (date != null) {
+      var text = date.getDate();
+      if (isExisting === true) {
+         attr = {"href": this.calendar.getCollection().href() +
+                         date.format(this.calendar.getHrefFormat())};
+         this.html.link(attr, text);
+      } else {
+         res.write(text);
+      }
+   }
+   this.html.closeTag("td");
+   return;
+};
+
+/**
+ * Renders a link to the previous or next month's calendar directly to response.
+ * @param {Date} date A date object set to the previous or next available
+ * month. This can be null in case there is no previous or next month.
+ */
+jala.Date.Calendar.Renderer.prototype.renderPrevNextLink = function(date) {
+   if (date != null) {
+      var attr = {"href": this.calendar.getCollection().href() +
+                          date.format(this.calendar.getHrefFormat())};
+      this.html.link(attr, date.format("MMMM", this.calendar.getLocale()));
+   }
+   return;
+};
+
+/**
+ * Renders the calendar directly to response.
+ * @param {Date} date A date object representing this calendar's month and year.
+ * Please mind that the day will be set to the <em>last</em> date in this
+ * month.
+ * @param {String} body The rendered calendar weeks including the day header
+ * (basically the whole kernel of the table).
+ * @param {Date} prevMonth A date object set to the last available date of
+ * the previous month. This can be used to render a navigation link to
+ * the previous month.
+ * @param {Date} nextMonth A date object set to the first available date
+ * of the next month. This can be used to render a navigation link to
+ * the next month.
+ */
+jala.Date.Calendar.Renderer.prototype.renderCalendar = function(date, body, prevMonth, nextMonth) {
+   var locale = this.calendar.getLocale();
+   this.html.openTag("table", {"class": "calendar"});
+   this.html.openTag("thead");
+   this.html.openTag("tr");
+   this.html.openTag("th", {"colspan": 7});
+   res.write(date.format("MMMM", locale));
+   res.write(' ');
+   res.write(date.format("yyyy", locale));
+   this.html.closeTag("th");
+   this.html.closeTag("tr");
+   this.html.closeTag("thead");
+   this.html.element("tbody", body);
+   this.html.openTag("tfoot");
+   this.html.openTag("tr");
+   this.html.openTag("td", {"class": "left", "colspan": 3});
+   this.renderPrevNextLink(prevMonth);
+   this.html.closeTag("td");
+   this.html.openTag("td");
+   this.html.closeTag("td");
+   this.html.openTag("td", {"class": "right", "colspan": 3});
+   this.renderPrevNextLink(nextMonth);
+   this.html.closeTag("td");
+   this.html.closeTag("tr");
+   this.html.closeTag("tfoot");
+   this.html.closeTag("table");
+   return;
 };
 
 /**
